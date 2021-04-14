baad8469d51d358e1ed0c5165c92eeb7e4f19849
ekucukkirli <56414732+ekucukkirli@users.noreply.github.com>
2021-04-14 05:48:36 +0300

    complete home page

--- dist/js/script.js
+++ dist/js/script.js
@@ -53,3 +53,47 @@ $(document).ready(function() {
 });
 
 // Rotate finish
+
+// Counter start
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
+
+// Counter finish
+
+
+// back-to-top
+if ($('#back-to-top').length) {
+  var scrollTrigger = 100, // px
+      backToTop = function () {
+          var scrollTop = $(window).scrollTop();
+          if (scrollTop > scrollTrigger) {
+              $('#back-to-top').addClass('show');
+          } else {
+              $('#back-to-top').removeClass('show');
+          }
+      };
+  backToTop();
+  $(window).on('scroll', function () {
+      backToTop();
+  });
+  $('#back-to-top').on('click', function (e) {
+      e.preventDefault();
+      $('html,body').stop().animate({
+          scrollTop: 0
+      }, 700);
+  });
+}
