999f61b38b25bcff44c6dcaf30247c4989216602
ekucukkirli <56414732+ekucukkirli@users.noreply.github.com>
2021-05-02 16:31:50 +0300

    js file regulations

--- src/js/script.js
+++ src/js/script.js
@@ -98,28 +98,17 @@ if ($('#back-to-top').length) {
   });
 }
 
-// Swiper
-// var swiper = new Swiper('.swiper-container', {
-//   pagination: {
-//     el: '.swiper-pagination',
-//     dynamicBullets: true,
-//   },
-// });
+// Carousel
+
 const swiper = new Swiper('.swiper-container', {
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
